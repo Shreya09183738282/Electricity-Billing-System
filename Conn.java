7f4d4dd5ba7e23780ef97e59d0ba57e8fe48013b
Shreya09183738282 <104248281+Shreya09183738282@users.noreply.github.com>
2023-06-14 14:00:03 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "Shreya1234$");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
